1636064bdac7cb9d79d7fb2ef8192a815fb7c56a
adamszabolcs <szabolcs.adam91@gmail.com>
2018-12-11 10:32:40 +0100

    started implementing interface methods

--- src/main/java/com/codecool/dartvaders/dao/implementation/PlayerDaoJdbc.java
+++ src/main/java/com/codecool/dartvaders/dao/implementation/PlayerDaoJdbc.java
@@ -1,6 +1,27 @@
 package com.codecool.dartvaders.dao.implementation;
 
 import com.codecool.dartvaders.dao.PlayerDao;
+import com.codecool.dartvaders.model.Player;
 
 public class PlayerDaoJdbc implements PlayerDao {
+
+    @Override
+    public void register(Player player) {
+
+    }
+
+    @Override
+    public boolean isUsernameTaken(String username) {
+        return false;
+    }
+
+    @Override
+    public String getHashByUsername(String username) {
+        return null;
+    }
+
+    @Override
+    public Player getCostumerByUsername(String username) {
+        return null;
+    }
 }
