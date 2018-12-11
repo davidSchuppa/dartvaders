285909446df525b77c1f600105a1e1856a18ac48
David Schuppa <d.schuppa@gmail.com>
2018-12-11 17:02:53 +0100

    refactoring class names, extend doTheGame.js

--- src/main/webapp/static/javascript/doTheGame.js
+++ src/main/webapp/static/javascript/doTheGame.js
@@ -3,14 +3,30 @@ function setEventListeners() {
 }
 
 function letsPlay() {
-    let playerOne = document.getElementById("player-one").value;
-    let playerTwo = document.getElementById("player-two").value;
+    let playerOne = document.getElementById("p1-name").value;
+    let playerTwo = document.getElementById("p2-name").value;
     let gametype = document.getElementById("gametype").value;
-    let playerOneTable = document.getElementById("player-one-table");
-    let playerTwoTable = document.getElementById("player-two-table");
+    let playerOneTable = document.getElementById("p1-score");
+    let playerTwoTable = document.getElementById("p2-score");
+
+
     playerTwoTable.innerText += gametype;
     playerOneTable.innerText += gametype;
 }
+
+// appendToElement: function (elementToExtend, textToAppend, prepend = false) {
+//     let fakeDiv = document.createElement('div');
+//     fakeDiv.innerHTML = textToAppend.trim();
+//     for (childNode of fakeDiv.childNodes) {
+//         if (prepend) {
+//             elementToExtend.prepend(childNode);
+//         } else {
+//             elementToExtend.appendChild(childNode);
+//         }
+//     }
+//
+//     return elementToExtend.lastChild;
+// },
 
 setEventListeners();
 
