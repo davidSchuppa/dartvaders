891872fb6d94a71bded95020eb28702d012cb5aa
adamszabolcs <szabolcs.adam91@gmail.com>
2018-12-11 10:32:19 +0100

    created interface methods for players

--- src/main/java/com/codecool/dartvaders/dao/PlayerDao.java
+++ src/main/java/com/codecool/dartvaders/dao/PlayerDao.java
@@ -1,4 +1,13 @@
 package com.codecool.dartvaders.dao;
 
+import com.codecool.dartvaders.model.Player;
+
 public interface PlayerDao {
+
+    void register(Player player);
+    boolean isUsernameTaken(String username);
+    String getHashByUsername(String username);
+    Player getCostumerByUsername(String username);
+
+
 }
