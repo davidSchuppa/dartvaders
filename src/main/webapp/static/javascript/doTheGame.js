c45b5d5a30b055890a08cd8f61caf20107e12720
adamszabolcs <szabolcs.adam91@gmail.com>
2018-12-11 14:58:26 +0100

    minor frontend changes

--- src/main/webapp/static/javascript/doTheGame.js
+++ src/main/webapp/static/javascript/doTheGame.js
@@ -5,8 +5,11 @@ function setEventListeners() {
 function letsPlay() {
     let playerOne = document.getElementById("player-one").value;
     let playerTwo = document.getElementById("player-two").value;
-    console.log(playerOne);
-    console.log(playerTwo);
+    let gametype = document.getElementById("gametype").value;
+    let playerOneTable = document.getElementById("player-one-table");
+    let playerTwoTable = document.getElementById("player-two-table");
+    playerTwoTable.innerText += gametype;
+    playerOneTable.innerText += gametype;
 }
 
 setEventListeners();
