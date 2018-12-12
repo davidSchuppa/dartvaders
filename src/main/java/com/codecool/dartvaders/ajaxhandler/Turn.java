dfeb128261fc31f47780188138611af4004648b6
adamszabolcs <szabolcs.adam91@gmail.com>
2018-12-12 09:05:47 +0100

    created turn ajaxhandler, get game from session, actualscore from javascript

--- src/main/java/com/codecool/dartvaders/ajaxhandler/Turn.java
+++ src/main/java/com/codecool/dartvaders/ajaxhandler/Turn.java
@@ -1,4 +1,25 @@
 package com.codecool.dartvaders.ajaxhandler;
 
-public class Turn {
+import com.codecool.dartvaders.dao.implementation.PlayerDaoJdbc;
+import com.codecool.dartvaders.model.Game;
+import com.codecool.dartvaders.model.Player;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = {"/turn"})
+public class Turn extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession(false);
+        Game game = (Game) session.getAttribute("game");
+        int actualScore = Integer.parseInt(req.getParameter("actualScore"));
+    }
+
 }
