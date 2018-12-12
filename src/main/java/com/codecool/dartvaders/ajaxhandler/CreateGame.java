55d0a0ed9adcb75bba4b729c125fc501ce8ade96
David Schuppa <d.schuppa@gmail.com>
2018-12-12 17:16:08 +0100

    players scores now counted individually, highest turns now tracked in board.js

--- src/main/java/com/codecool/dartvaders/ajaxhandler/CreateGame.java
+++ src/main/java/com/codecool/dartvaders/ajaxhandler/CreateGame.java
@@ -27,12 +27,11 @@ public class CreateGame extends HttpServlet {
 
         Player player1 = new Player(req.getParameter("p1Name"));
         Player player2 = new Player(req.getParameter("p2Name"));
-        System.out.println(req.getParameter("p2Name"));
+//        System.out.println(req.getParameter("p2Name"));
         String gameType = req.getParameter("gametype");
         List<Player> players = new ArrayList<>(Arrays.asList(player1, player2));
         int legs = Integer.parseInt(req.getParameter("legs"));
         Game game = new Game(legs, players, gameType);
-        System.out.println("Created game");
         HttpSession session = req.getSession(true);
         session.setAttribute("game", game);
     }
--- src/main/webapp/static/javascript/gameelements/game.js
+++ src/main/webapp/static/javascript/gameelements/game.js
@@ -1,10 +1,11 @@
 let game = {
     _doubles: 0,
     _triples: 0,
-    _highestTurn: 0,
     _leg: 0,
     _round: 0,
     _turnCounter: 0,
+    _p1HighestTurn: 0,
+    _p2HighestTurn: 0,
     _actualPlayer: "p1",
     _turnScore: 0,
     _pointRemaining: document.getElementById("gametype").value,
@@ -26,37 +27,50 @@ let game = {
 
     sendInformation: function (p1Name, p2Name, gametype, legs) {
         console.log("Game created.");
-        $.post("/create-game", {p1Name:p1Name, p2Name:p2Name, gametype:gametype, legs:legs});
+        $.post("/create-game", {p1Name: p1Name, p2Name: p2Name, gametype: gametype, legs: legs});
     },
 
     registerTurn: function (score) {
         game._turnCounter++;
-        console.log(game._turnCounter);
+        console.log("turn counter: " + game._turnCounter);
 
         let playerOriginalScore = parseInt(document.getElementById(game._actualPlayer + "-score").innerText);
-
-            console.log(playerOriginalScore);
+        let playerScoreDiv = document.getElementById(game._actualPlayer + "-score");
         if (game.isThrowValid(playerOriginalScore, score)) {
+            game._turnScore += score;
+            console.log("Turn score: " + game._turnScore);
+            game._pointRemaining = playerOriginalScore - score;
+            playerScoreDiv.innerText = game._pointRemaining;
+            if (game.checkWin()) {
+                console.log(game._actualPlayer + " won");
+            }
         }
+        game.setHighestTurn();
         game.changePlayer();
-        console.log(game._actualPlayer);
+        console.log("Actual player: " + game._actualPlayer);
+        console.log("P1 highest turn: " + game._p1HighestTurn);
+        console.log("P2 highest turn: " + game._p2HighestTurn);
     },
 
     isThrowValid: function (originalPoints, throwScore) {
-        return (originalPoints - throwScore) >= 2;
+        return (originalPoints - throwScore) >= 2 || (originalPoints - throwScore == 0);
     },
 
     outOfBoard: function () {
         console.log("Throw is out of board");
     },
 
+    checkWin: function () {
+        return game._pointRemaining === 0;
+    },
+
     changePlayer: function () {
         if (game._turnCounter === 3) {
-            if (game._actualPlayer == "p1") {
-                game._actualPlayer == "p2";
+            if (game._actualPlayer === "p1") {
+                game._actualPlayer = "p2";
                 game.revertTurnStats();
             } else {
-                game._actualPlayer == "p1";
+                game._actualPlayer = "p1";
                 game.revertTurnStats();
             }
         }
@@ -65,13 +79,18 @@ let game = {
     revertTurnStats: function () {
         game._pointRemaining = document.getElementById(game._actualPlayer + "-score");
         game._turnCounter = 0;
-        game.setHighestTurn();
         game._turnScore = 0;
     },
 
     setHighestTurn: function () {
-        if (game._turnScore > game._highestTurn) {
-            game._highestTurn = game._turnScore;
+        if (game._actualPlayer == "p1") {
+            if (game._turnScore > game._p1HighestTurn) {
+                game._p1HighestTurn = game._turnScore;
+            }
+        } else {
+            if (game._turnScore > game._p2HighestTurn) {
+                game._p2HighestTurn = game._turnScore;
+            }
         }
     }
 
